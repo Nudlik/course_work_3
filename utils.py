b6712ff69ee8fe0fd9f9cb2843b3766eb318c7fa
Nudlik <gegorp@mail.com>
2023-08-12 06:08:32 +0700

    refactor utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
-def sort_transactions(transactions, count=5, sort_by='date', state='state'):
-    return sorted(transactions, key=lambda x: x[sort_by] if x and x[state] == 'EXECUTED' else '', reverse=True)[:count]
+def sort_transactions(transactions, count=5, sort_by='date', state='EXECUTED'):
+    """
+    Сортировка списка со словарями
+    :param transactions: список словарей
+    :param count: какая нужна длинна
+    :param sort_by: сортировка по
+    :param state: условие EXECUTED - пройденные операции, CANCELED - отмененныу
+    :return: список отсортированных словарей
+    """
+    return sorted(transactions, key=lambda x: x[sort_by] if x and x['state'] == state else '', reverse=True)[:count]
 
 
 if __name__ == '__main__':
