af3da304412096aed5bbd56a864cae68dfbd5e84
Nudlik <gegorp@mail.com>
2023-08-12 04:04:10 +0700

    refactor main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import utils
+from models import ReadJson, Transaction
+from settings import PATH_TO_JSON
 
 
 def main():
-    pass
+    transactions = ReadJson.load_json(PATH_TO_JSON)
+    sorted_transactions = utils.sort_transactions(transactions)
+    transactions_list = [Transaction(transaction) for transaction in sorted_transactions]
+    for i in transactions_list:
+        print(i, '\n')
 
 
 if __name__ == '__main__':
