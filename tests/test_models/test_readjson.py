05ac2daef9ecabd8fe70a85aa08796c416102ab4
Nudlik <gegorp@mail.com>
2023-08-12 11:31:33 +0700

    refactor test_read_json

---
+++ tests/test_models/test_readjson.py
@@ -0,0 +1,9 @@
+import os
+from models.readjson import ReadJson
+
+
+def test_read_json(test_fixture_trans_good, test_fixture_trans_not_good):
+    path = os.path.join(os.path.dirname(__file__), '../test_data/test_json.json')
+    path_bad = os.path.join(os.path.dirname(__file__), 'test_json1.json')
+    assert ReadJson.load_json(path) == [test_fixture_trans_good, test_fixture_trans_not_good]
+    assert ReadJson.load_json(path_bad) is None
