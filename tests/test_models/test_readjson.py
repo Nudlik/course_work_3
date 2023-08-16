4578df1595405423fd8b8c6a95102cc4804cebbe
Nudlik <gegorp@mail.com>
2023-08-16 11:29:27 +0700

    fix tests with raise

--- tests/test_models/test_readjson.py
+++ tests/test_models/test_readjson.py
@@ -1,4 +1,7 @@
 import os
+
+import pytest
+
 from models.readjson import ReadJson
 
 
@@ -6,4 +9,5 @@ def test_read_json(test_fixture_trans_good, test_fixture_trans_not_good):
     path = os.path.join(os.path.dirname(__file__), '../test_data/test_json.json')
     path_bad = os.path.join(os.path.dirname(__file__), 'test_json1.json')
     assert ReadJson.load_json(path) == [test_fixture_trans_good, test_fixture_trans_not_good]
-    assert ReadJson.load_json(path_bad) is None
+    with pytest.raises(FileNotFoundError):
+        assert ReadJson.load_json(path_bad)
